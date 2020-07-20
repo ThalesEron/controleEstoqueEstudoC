fb228aea6a00d0d2fc877f021b2395a1cff7c048
ThalesEron <thales_cardoso96@outlook.com>
2020-07-20 16:04:15 -0300

    programando campos
    >Acrescentado Código de receber e mostrar os textos dos campos.
    
    >Programado botão de limpar campos.
    
    >Verificador de campo vazio.

--- projetoControleEstoque/ControleEstoque/mainwindow.cpp
+++ projetoControleEstoque/ControleEstoque/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,5 +12,43 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+
+
 }
 
+
+void MainWindow::on_confirmRegisterButton_clicked()
+{
+    //pegando o texto escrito no campo
+    QString nome = ui->lineEditName->text();
+    QString quantidade = ui->lineEditQuant->text();
+    QString tipo = ui->lineEditType->text();
+    QString fornecedor = ui->lineEditForne->text();
+    QString peso = ui->lineEditPeso->text();
+
+
+
+    //verificar se tem algum campo vazio
+    if (nome == "" || quantidade == "" || tipo == "" || fornecedor == "" || peso == "") {
+        QMessageBox::information(this, "Registros Informados:", "Digite um valor em todos campos.");
+        return;
+    }
+
+
+    //mostrando os textos digitados
+    QMessageBox::information(this, "Registros Informados:", "Nome: "+nome+"\nQuantidade: "+quantidade+"\nTipo: "+tipo+"Fornecedor: "+fornecedor+"\nPeso: "+peso+".");
+
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    //limpando os campos digitados
+    ui->lineEditName->clear();
+    ui->lineEditQuant->clear();
+    ui->lineEditType->clear();
+    ui->lineEditForne->clear();
+    ui->lineEditPeso->clear();
+
+    //colocando o cursor no campo de texto de nome
+    ui->lineEditName->setFocus();
+}
